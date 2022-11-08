8470da29ff8968749aaadd548f92284b2cb9acec
Adam Fanslau <adam.fanslau@gmail.com>
2022-11-08 20:11:37 +0000

    Added post lounch functionality

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,40 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer ES1',
+    launchDate: new Date('December 27, 2030'),
+    target: 'Kepler-442 b',
+    customers: [
+        'ZTM',
+        'NASA'
+    ],
+    upcoming: true,
+    success: true
+};
+
+launches.set(launch.flightNumber, launch);
+
+function getAllLaunches () {
+    return Array.from(launches.values());
+}
+
+function addNewLaunch (launch) {
+    latestFlightNumber += 1;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            success:true,
+            upcoming: true,
+            customers: ['ZTM', 'NASA'],
+            flightNumber: latestFlightNumber
+    }));
+}
+
+module.exports = {
+    getAllLaunches,
+    addNewLaunch
+};
