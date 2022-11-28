6aab8fd8d8a982604fe036a4988e3e762c78721e
Adam Fanslau <adam.fanslau@gmail.com>
2022-11-28 19:09:56 +0000

    Added api versioning

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
 
-describe('Launches API', () => {
+describe('Launches API v1', () => {
     beforeAll(async () => {
         await mongoConnect();
     });
@@ -11,16 +11,16 @@ describe('Launches API', () => {
         await mongoDisconnect();
     });
 
-    describe('Test GET /launches', () => {
+    describe('Test GET /v1/launches', () => {
         test('It should respond with 200 success', async () => {
             const response = await request(app)
-                .get('/launches')
+                .get('/v1/launches')
                 .expect('Content-Type', /json/)
                 .expect(200);
         });
     });
     
-    describe('Test POST /launch', () => {
+    describe('Test POST /v1/launch', () => {
         const completeLaunchData = {
             mission: 'Kepler Exploration X',
             rocket: 'Explorer ES1',
@@ -43,7 +43,7 @@ describe('Launches API', () => {
     
         test('It should respond with 201 success', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(completeLaunchData)
                 .expect('Content-Type', /json/)
                 .expect(201);
@@ -57,7 +57,7 @@ describe('Launches API', () => {
     
         test('It should catch missing required properties', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithoutDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
@@ -69,7 +69,7 @@ describe('Launches API', () => {
     
         test('It should catch invalid dates', async () => {
             const response = await request(app)
-                .post('/launches')
+                .post('/v1/launches')
                 .send(launchDataWithIncorrectDate)
                 .expect('Content-Type', /json/)
                 .expect(400);
