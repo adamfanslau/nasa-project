6501fa68013467ed13e1773dd88d5130b5a3e392
Adam Fanslau <adam.fanslau@gmail.com>
2022-11-29 18:45:07 +0000

    added pagination to the api

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,22 +5,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-    flightNumber: 100, // flight_number
-    mission: 'Kepler Exploration X', // name
-    rocket: 'Explorer ES1', // rocket.name
-    launchDate: new Date('December 27, 2030'), // date_local
-    target: 'Kepler-442 b', // not applicable
-    customers: [
-        'ZTM',
-        'NASA'
-    ], // payloads.customers for each payload
-    upcoming: true, // upcoming
-    success: true // success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
 async function populateLaunches () {
@@ -109,11 +93,14 @@ async function getLatestFlightNumber() {
     return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches () {
+async function getAllLaunches (skip, limit) {
     return await launchesDatabase.find({}, {
         '_id': 0,
         '__v': 0
-    });
+    })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
