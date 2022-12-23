34b0e9d0b96dd10f5dca7cc7d9409a60496785e6
Adam Fanslau <adam.fanslau@gmail.com>
2022-12-23 11:56:38 +0000

    Populate planets for tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const {loadPlanetsData} = require('../../models/planets.model');
 
 describe('Launches API v1', () => {
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
